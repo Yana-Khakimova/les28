e1c8388cb1e9c8ca8edf017975777444f6e4f0cc
Yana Khakimova <yana.saitova@mail.ru>
2023-04-23 22:25:17 +0300

    create project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        var cats = Cat.makeCats(10);
+        Printer.print(cats);
+
+        // А сюда добавьте код, который будет сортировать коллекцию котов
+        // используйте лямбда-выражения и ссылки на методы
+        // cats.sort(?);
+        // Printer.print(cats);
+    }
+}
